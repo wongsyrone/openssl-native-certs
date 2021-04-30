6d12be7ab23103dca8045a97c1a7f6ac4139216b
Syrone Wong <wong.syrone@gmail.com>
2021-04-30 18:23:31 +0800

    unix: should compile now
    use std::io::Read as pem files are small and not processed frequently
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- src/unix.rs
+++ src/unix.rs
@@ -2,14 +2,15 @@ use crate::RootStoreBuilder;
 use openssl_probe;
 use std::{
     fs::File,
-    io::{BufReader, Error, ErrorKind},
+    io::{Error, ErrorKind},
     path::Path,
 };
 
 fn load_file(builder: &mut impl RootStoreBuilder, path: &Path) -> Result<(), Error> {
-    let f = File::open(&path)?;
-    let mut f = BufReader::new(f);
-    if builder.load_pem_file(&mut f).is_err() {
+    let mut f = File::open(&path)?;
+    let mut buf: Vec<u8> = Vec::new();
+    std::io::Read::read_to_end(&mut f, &mut buf)?;
+    if builder.load_pem(&buf).is_err() {
         Err(Error::new(
             ErrorKind::InvalidData,
             format!("Could not load PEM file {:?}", path),
